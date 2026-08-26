93ac2ce87dfd6cbb110b067242ba4971ad7e8af3
shinny0wl <paulina.floress@iteso.mx>
2026-08-26 10:28:54 -0600

    Arregle el error de la funcion add y agregue la funcion random_test, junto con su prueba

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
+import random
+import hashlib
+
+
 # Funcion para sumar dos numeros
 def add(x, y):
     return x + y
 
 
+def random_hash():
+    # Genera un valor aleatorio SHA-256 hash (un valor hexadecimal)
+    random_value = str(random.random())
+    return hashlib.sha256(random_value.encode()).hexdigest()
+
+
 if __name__ == "__main__":
     print(add(1, 1))
+    print(random_hash())
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,13 @@
 from hello import add
+from hello import random_hash
 
 
 def test_add():
-    assert add(1, 1) == 4
+    assert add(1, 1) == 2
+
+
+def test_random_hash():
+    result = random_hash()
+
+    assert isinstance(result, str)
+    assert len(result) == 64
